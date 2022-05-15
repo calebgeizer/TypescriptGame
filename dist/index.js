bbc74f333f78c0951375c6435c41b0066b396908
Caleb Geizer <caleb.geizer@gmail.com>
2022-05-15 19:35:12 +1000

    add render

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,26 @@
-console.log('Hello World');
+var GameSpace = /** @class */ (function () {
+    function GameSpace(width, height) {
+        if (width === void 0) { width = 1920; }
+        if (height === void 0) { height = 1080; }
+        this.render(this.width, this.height);
+    }
+    GameSpace.prototype.render = function (x, y) {
+        var gamearea = this.createDiv(x, y, "white", "game");
+        gamearea.style.margin = "auto";
+        document.body.appendChild(gamearea);
+    };
+    GameSpace.prototype.createDiv = function (x, y, color, tag) {
+        var el = document.createElement('div');
+        el.style.width = x + "px";
+        el.style.height = y + "px";
+        el.style.background = color;
+        el.id = tag;
+        return el;
+    };
+    return GameSpace;
+}());
+var game = new GameSpace();
+// TODO
+//// - Create Function for JS to Render HTML
+// - Define Class Structure
+// - Define Project Scope
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello World');
+class GameSpace {
+    width:number;
+    height:number;
+   
+    constructor(width = 1920, height = 1080) {
+      this.render(this.width,this.height);
+    }
+
+    render(x,y){
+        const gamearea = this.createDiv(x,y,"white","game");
+        gamearea.style.margin = "auto";
+        
+        document.body.appendChild(gamearea);
+    }
+
+    createDiv(x,y,color,tag){
+        const el = document.createElement('div');
+        el.style.width = x+"px";
+        el.style.height = y+"px";
+        el.style.background = color;
+        el.id = tag;
+        return el;
+    }
+}
+
+const game = new GameSpace();
+
+// TODO
+//// - Create Function for JS to Render HTML
+// - Define Class Structure
+// - Define Project Scope
