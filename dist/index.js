d2b3515ef55b232d8c2ab3a94c9614f06ca17b80
calebgeizer <caleb.geizer@gmail.com>
2022-05-18 23:31:55 +1000

    Add SVG Primitives

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,72 @@
 var GameSpace = /** @class */ (function () {
     function GameSpace(width, height) {
-        if (width === void 0) { width = 1920; }
-        if (height === void 0) { height = 1080; }
-        this.render(this.width, this.height);
+        if (width === void 0) { width = 1280; }
+        if (height === void 0) { height = 720; }
+        this.render(width, height);
     }
     GameSpace.prototype.render = function (x, y) {
-        var gamearea = this.createDiv(x, y, "white", "game");
+        var gamearea = this.createSVG(x, y);
         gamearea.style.margin = "auto";
+        var rectangle = this.createEllipse(200, 100, 100, 100, "red");
         document.body.appendChild(gamearea);
+        document.getElementsByTagName("svg")[0].appendChild(rectangle);
     };
-    GameSpace.prototype.createDiv = function (x, y, color, tag) {
-        var el = document.createElement('div');
-        el.style.width = x + "px";
-        el.style.height = y + "px";
-        el.style.background = color;
-        el.id = tag;
-        return el;
+    GameSpace.prototype.getSvgNS = function () {
+        var svgns = "http://www.w3.org/2000/svg", container = document.getElementById('cont');
+        return svgns;
+    };
+    GameSpace.prototype.createSVG = function (x, y) {
+        var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("width", x + "px");
+        svg.setAttribute("height", y + "px");
+        return svg;
+    };
+    // Create SVG Elements
+    GameSpace.prototype.createLine = function (x1, y1, x2, y2, color) {
+        var svgns = this.getSvgNS();
+        var line = document.createElementNS(svgns, "line");
+        line.setAttribute("x1", x1);
+        line.setAttribute("y1", y1);
+        line.setAttribute("x2", x2);
+        line.setAttribute("y2", y2);
+        line.setAttribute("style", "stroke:" + color);
+        return line;
+    };
+    GameSpace.prototype.createRect = function (x, y, color) {
+        var svgns = this.getSvgNS();
+        var rect = document.createElementNS(svgns, "rect");
+        rect.setAttribute("width", x);
+        rect.setAttribute("height", y);
+        rect.setAttribute("style", "fill:" + color);
+        return rect;
+    };
+    GameSpace.prototype.createCircle = function (x, y, r, color) {
+        var svgns = this.getSvgNS();
+        var circle = document.createElementNS(svgns, "circle");
+        circle.setAttribute("cx", x);
+        circle.setAttribute("cy", y);
+        circle.setAttribute("r", r);
+        circle.setAttribute("style", "fill:" + color);
+        return circle;
+    };
+    GameSpace.prototype.createEllipse = function (x, y, rx, ry, color) {
+        var svgns = this.getSvgNS();
+        var ellipse = document.createElementNS(svgns, "ellipse");
+        ellipse.setAttribute("cx", x);
+        ellipse.setAttribute("cy", y);
+        ellipse.setAttribute("rx", rx);
+        ellipse.setAttribute("ry", ry);
+        ellipse.setAttribute("style", "fill:" + color);
+        return ellipse;
+    };
+    GameSpace.prototype.createText = function (x, y, text1, color) {
+        var svgns = this.getSvgNS();
+        var text = document.createElementNS(svgns, "text");
+        text.setAttribute("x", x);
+        text.setAttribute("y", y);
+        text.setAttribute("fill", color);
+        text.innerHTML = text1;
+        return text;
     };
     return GameSpace;
 }());
@@ -24,3 +75,4 @@ var game = new GameSpace();
 //// - Create Function for JS to Render HTML
 // - Define Class Structure
 // - Define Project Scope
+//! - Create path, polyline, polygon
--- src/index.ts
+++ src/index.ts
@@ -2,25 +2,90 @@ class GameSpace {
     width:number;
     height:number;
    
-    constructor(width = 1920, height = 1080) {
-      this.render(this.width,this.height);
+    constructor(width = 1280, height = 720) {
+      this.render(width,height);
     }
 
     render(x,y){
-        const gamearea = this.createDiv(x,y,"white","game");
+        const gamearea = this.createSVG(x,y);
         gamearea.style.margin = "auto";
+        const rectangle = this.createEllipse(200,100,100,100,"red");
         
         document.body.appendChild(gamearea);
+        document.getElementsByTagName("svg")[0].appendChild(rectangle);
     }
 
-    createDiv(x,y,color,tag){
-        const el = document.createElement('div');
-        el.style.width = x+"px";
-        el.style.height = y+"px";
-        el.style.background = color;
-        el.id = tag;
-        return el;
+    getSvgNS(){
+        const svgns = "http://www.w3.org/2000/svg", 
+        container = document.getElementById( 'cont' );
+        return svgns;
     }
+
+    createSVG(x,y){
+        const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("width",x+"px");
+        svg.setAttribute("height",y+"px");
+        return svg;
+    }
+
+    // Create SVG Elements
+
+    createLine(x1,y1,x2,y2,color){
+        var svgns = this.getSvgNS();
+        const line = document.createElementNS(svgns,"line");
+
+        line.setAttribute("x1",x1);
+        line.setAttribute("y1",y1);
+        line.setAttribute("x2",x2);
+        line.setAttribute("y2",y2);
+        line.setAttribute("style","stroke:"+color);
+        return line;
+    }
+
+    createRect(x,y,color){
+        var svgns = this.getSvgNS();
+        const rect = document.createElementNS(svgns,"rect");
+
+        rect.setAttribute("width",x);
+        rect.setAttribute("height",y);
+        rect.setAttribute("style","fill:"+color);
+        return rect;
+    }
+
+    createCircle(x,y,r,color){
+        var svgns = this.getSvgNS();
+        const circle = document.createElementNS(svgns,"circle");
+
+        circle.setAttribute("cx",x);
+        circle.setAttribute("cy",y);
+        circle.setAttribute("r",r);
+        circle.setAttribute("style","fill:"+color);
+        return circle;
+    }
+
+    createEllipse(x,y,rx,ry,color){
+        var svgns = this.getSvgNS();
+        const ellipse = document.createElementNS(svgns,"ellipse");
+
+        ellipse.setAttribute("cx",x);
+        ellipse.setAttribute("cy",y);
+        ellipse.setAttribute("rx",rx);
+        ellipse.setAttribute("ry",ry);
+        ellipse.setAttribute("style","fill:"+color);
+        return ellipse;
+    }
+
+    createText(x,y,text1,color){
+        var svgns = this.getSvgNS();
+        const text = document.createElementNS(svgns,"text");
+
+        text.setAttribute("x",x);
+        text.setAttribute("y",y);
+        text.setAttribute("fill",color);
+        text.innerHTML = text1;
+        return text;
+    }
+
 }
 
 const game = new GameSpace();
@@ -29,3 +94,5 @@ const game = new GameSpace();
 //// - Create Function for JS to Render HTML
 // - Define Class Structure
 // - Define Project Scope
+
+//! - Create path, polyline, polygon
